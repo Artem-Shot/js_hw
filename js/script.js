d3020498b997e5bfeb4c64381bc98bc8353d4705
Artem <artemshotskiy@gmail.com>
2024-08-03 14:02:39 +0300

    hw1.2 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const firstNumber = prompt('Enter first number');
+const secondNumber = prompt('Enter second number');
+
+
+
+
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(firstNumber / secondNumber);
+console.log(firstNumber * secondNumber);
