544664c4add0ee1c84ef6d43662ba7b3ab8841e5
Artem <artemshotskiy@gmail.com>
2024-08-03 14:52:16 +0300

    hw2 half

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const name = prompt('What is your name?');
+const age  = prompt('What is your age?');
+let likesProgramming = confirm('Do you like programming?');
+
+
+console.log(name);
+console.log(age);
+console.log(likesProgramming);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof likesProgramming);
+
+let greeting = (`Hello, + ${name}!`).trim();
+
+alert(greeting);
