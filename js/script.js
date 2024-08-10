d59586da6266421a5b3d5bf0d57aef5749f3569e
Artem <artemshotskiy@gmail.com>
2024-08-10 14:50:42 +0300

    hw3 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
+
+let sum = (num1 + num2 + num3)/ 3
+
+alert(sum);
