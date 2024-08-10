9f0c09fdafcdf32959cce8a24aea6fef215b53f4
Artem <artemshotskiy@gmail.com>
2024-08-10 15:07:49 +0300

    hw3.1 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const hour = +prompt('Введіть кількість годин');
+const minutes = hour*60;
+const seconds = hour*3600;
+
+alert('Це ' + minutes + ' хвилин' + ' Або ' + seconds + ' секунд')
