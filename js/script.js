70ea94f7dde0432c245755d277e9c2bf73bca2ba
Artem <artemshotskiy@gmail.com>
2024-08-21 17:03:23 +0300

    hw-5.1 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,53 @@
 'use strict';
 
+// let string = '';
+// for (let i = 20; i <= 30; i += 0.5) {
+//   string += `${i} `;
+// }
+// console.log(string.trim());
+//
+// const curancyRate = 27;
+// for (let i = 10; i <= 100; i += 10) {
+//   const sum = i * curancyRate;
+//   console.log(`${i} доларів коштують ${sum} гривеь`);
+// }
+
+// const num = 1000;
+//
+// for (let i = 1; i <= 100; i++) {
+//   if (i * i >= num) {
+//     break;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// const num = 5;
+// if (typeof (num) === 'number' && !isNaN(num) && isFinite(num)) {
+//   if (num < 1) {
+//     console.log('error');
+//   }
+//   if (num === 1) {
+//     console.log('number is 1');
+//   }
+// }
+// for (let i = 2; i < num; i++) {
+//   if (num % i !== 0) {
+//     console.log(num);
+//   }
+// }
+
+// function isLessOf3(num) {
+//   if (typeof (num) === 'number' && !isNaN(num) && isFinite(num)) {
+//     if (num < 1) return false;
+//   }
+//   while (num % 3 === 0) {
+//     num /= 3;
+//   }
+//   return num === 1;
+// }
+// console.log(isLessOf3(0));
+// console.log(isLessOf3(9));
+// console.log(isLessOf3(81));
+// console.log(isLessOf3(13));
+// console.log(isLessOf3(NaN));
