3acd667da198b8e217bc81d727fc590eab1054e1
Artem <artemshotskiy@gmail.com>
2024-08-22 12:23:42 +0300

    hw-5.1 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+function generateKey(length, characters) {
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const randomSymbol = Math.floor(Math.random() * characters.length);
+    key += characters[randomSymbol];
+  }
+
+  return key;
+}
+console.log(key);
