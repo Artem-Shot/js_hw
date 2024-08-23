9499c2165d9ac0a0eabd4d411e556ecb2a3954ac
Artem <artemshotskiy@gmail.com>
2024-08-23 13:10:55 +0300

    hw-6.1 if completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+function padString(str, num, char, bool) {
+  if (typeof str !== 'string') {
+    return 'some error string';
+  }
+  if (typeof num !== 'number') {
+    return 'some error number';
+  }
+  if (typeof char !== 'string' || char.length !== 1) {
+    return 'some error length';
+  }
+  if (typeof bool !== 'boolean') {
+    return 'some error boolean';
+  }
+  if (num < str.length) { return str.substr(0, num); }
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
