714144abbcd32421562be9645b8bb1044dbfd6a8
Artem <artemshotskiy@gmail.com>
2024-09-09 15:28:55 +0300

    hw-8 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const arr = [7, 5, 3, 10, 15, 22, 101, 48, 159, 147];
+arr.sort((a, b) => a - b);
+const deletedElements = arr.splice(1, 3);
+console.log(arr);
+console.log(deletedElements);
