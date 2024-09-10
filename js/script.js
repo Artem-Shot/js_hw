8ed3ef9c488bf5ad4cf3856c1bcc8237453d4544
Artem <artemshotskiy@gmail.com>
2024-09-10 13:42:50 +0300

    hw-23 half

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const minBal = 2000;
+
+const removeSymbols = (str, symbToRemove) => {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (symbToRemove.includes(str[i])) continue;
+    result.push(str[i]);
+  }
+  return result.join('');
+};
+
+const phoneNumMinBalance = (arr, minBal = 0, balKey = 'balance') => {
+  arr.forEach((item) => {
+    const newString = Number(removeSymbols(item.balance, ['$', ',']));
+    console.log(newString);
+  });
+};
+
+const phoneNumbers = phoneNumMinBalance(users, minBal, phoneNumMinBalance);
+console.log(phoneNumbers);
+// const newArr = users.filter((item) => item.balance > minBal);
+// const sumOfAllBalances = 0;
+//
+// newArr.forEach((item) => { item.balance = sumOfAllBalances += item.balance; });
+// console.log(sumOfAllBalances);
