9e765db446a9c76bb117e82e25265935ed3b4248
Artem <artemshotskiy@gmail.com>
2024-09-17 13:50:40 +0300

    hw-24 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+/* eslint-disable*/
+const test = () => {
+  for (var i = 0; i < 3; i++) {
+    console.log('var ' + i);
+  }
+
+
+  for (let j = 0; j < 3; j++) {
+    console.log('let ' + j);
+  }
+
+  for (let k = 0; k < 3; k++) {
+    const qq = k + 5;
+    console.log('константа ' + qq);
+  }
+
+  console.log(i);
+  console.log(j);
+  console.log(k);
+}
+
+
+
+
+test()
+
+
+
