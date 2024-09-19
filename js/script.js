97a3489f2f350cb9bab67f57ab866f25212a4874
Artem <artemshotskiy@gmail.com>
2024-09-19 22:44:48 +0300

    hw-27 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const cars = [
+  {
+    brand: 'Jaguar',
+    price: 30000,
+    bodyStyle: 'sedan',
+  },
+  {
+    brand: 'Land Rover',
+    price: 80000,
+    bodyStyle: 'crosover',
+  },
+  {
+    brand: 'porsche',
+    price: 130000,
+    bodyStyle: 'coupe',
+  },
+];
+
+const getCarInfo = function (str) {
+  console.log(str);
+  console.log(`${this.brand} is ${this.bodyStyle} costs ${this.price}`);
+};
+
+for (const car of cars) {
+  const x = getCarInfo.bind(car, `This car costs ${car.price}`);
+  x();
+}
