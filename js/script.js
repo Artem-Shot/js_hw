537a54f6ed9d16f24fe721cd401ab5c4a6542e3e
Artem <artemshotskiy@gmail.com>
2024-09-20 16:07:10 +0300

    hw-28 corrected

--- js/script.js
+++ js/script.js
@@ -1,47 +1,22 @@
 'use strict';
 
-// const InsertWordInto = (string) => (word) => {
-//   const words = string.split(' ');
-//   const getMiddle = Math.floor(words.length / 2);
-//   const newString = [
-//     ...words.slice(0, getMiddle),
-//     word,
-//     ...words.slice(getMiddle),
-//   ].join(' ');
-//   return newString;
-// };
-// const insert = InsertWordInto('hello world');
-// console.log(insert());
-//
-// const result = insert('Odessa');
-// console.log(result);
-//
-// const result2 = insert('Odessa');
-// console.log(result2);
-//
-// const result3 = insert('Odesa');
-// console.log(result3);
-//
-// const result4 = insert('Odesa');
-// console.log(result4);
-
-const InsertWordInto = (str) => {
+const insertWordInto = (str) => {
   let num = 0;
 
   return (word) => {
     let result;
     const newString = str.split(' ');
 
-    if (num % 3 === 0) {
+    if (num === 0) {
       const getMiddle = Math.floor((newString.length - 1) / 2);
       result = [
         ...newString.slice(0, getMiddle + 1),
         word,
         ...newString.slice(getMiddle + 1),
       ].join(' ');
-    } else if (num % 3 === 1) {
+    } else if (num === 1) {
       result = `${word} ${str}`;
-    } else if (num % 3 === 2) {
+    } else if (num === 2) {
       result = `${str} ${word}`;
     }
 
@@ -50,7 +25,7 @@ const InsertWordInto = (str) => {
   };
 };
 
-const insert = InsertWordInto('hello world');
+const insert = insertWordInto('hello world');
 
 const result = insert('Odesa'); // -> 'hello Odesa world'
 console.log(result);
