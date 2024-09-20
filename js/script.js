fcbb7f6f735cc3e16816ffa517e3c340572027a2
Artem <artemshotskiy@gmail.com>
2024-09-20 13:29:56 +0300

    hw-28 done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const getFactorial = (number) => {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * getFactorial(number - 1);
+};
+
+const result = getFactorial(3);
+console.log(result);
+
+const pow = (num, degree) => {
+  if (num === 0 || degree === 0) {
+    return 1;
+  } return num * pow(num, degree - 1);
+};
+const res = pow(2, 6);
+console.log(res);
+
+const sum = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return sum(a + 1, b - 1);
+};
+
+console.log(sum(10, 15));
