be228df45dbe388aa3a75b3edb1ea9509f9649f8
Artem <artemshotskiy@gmail.com>
2024-09-23 10:12:24 +0300

    hw-26 corrected +

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ const insertWordInto = (str) => {
   let num = 0;
 
   return (word) => {
-    let result;
+    let res;
     const newString = str.split(' ');
 
     if (num === 0) {
       const getMiddle = Math.floor(newString.length / 2);
-      result = [
+      res = [
         ...newString.slice(0, getMiddle),
         word,
         ...newString.slice(getMiddle),
       ].join(' ');
     } else if (num === 1) {
-      result = `${word} ${str}`;
+      res = `${word} ${str}`;
     } else if (num === 2) {
-      result = `${str} ${word}`;
+      res = `${str} ${word}`;
     }
 
     num++;
-    return result;
+    return res;
   };
 };
 
