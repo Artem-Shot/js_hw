331462568daf1cd4e3e3325ff4d9e76e79a83145
Artem <artemshotskiy@gmail.com>
2024-09-24 16:11:13 +0300

    hw-30 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const sum = () => {
+  let step = 0;
+  return (result) => {
+    step += result;
+    return step;
+  };
+};
+
+const finalResult = sum();
+
+console.log(finalResult(3));
+console.log(finalResult(5));
+console.log(finalResult(20));
