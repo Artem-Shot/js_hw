643ec3154246ca7fe519549bacf50c8093835b1c
Artem <artemshotskiy@gmail.com>
2024-09-27 14:30:26 +0300

    hw-33 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+
+const random = Math.floor(Math.random() * images.length);
+
+const randomImage = images[random];
+
+console.log(randomImage);
+
+const newImage = document.createElement('img');
+newImage.src = randomImage;
