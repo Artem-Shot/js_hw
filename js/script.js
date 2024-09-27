38b9b6bb0205d7c67b3c3ceba227f3cd3511c83e
Artem <artemshotskiy@gmail.com>
2024-09-27 13:59:39 +0300

    hw-32 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const tableBody = document.getElementById('table-body');
+let number = 1;
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('tr');
+  for (let j = 0; j < 10; j++) {
+    const value = document.createElement('td');
+    value.textContent = number;
+    row.appendChild(value);
+    number++;
+  }
+  tableBody.appendChild(row);
+}
+
+setTimeout(() => {
+  tableBody.style.backgroundColor = 'lightblue';
+}, 1000);
+setTimeout(() => {
+  tableBody.style.backgroundColor = 'pink';
+}, 2000);
+setTimeout(() => {
+  tableBody.style.backgroundColor = 'white';
+  tableBody.style.fontWeight = 'bold';
+}, 4000);
+
+const bodyEl = document.body;
+bodyEl.style.backgroundColor = 'purple';
