b3143f424f56450f9aa755a28cbb1dfd4ebc5364
Artem <artemshotskiy@gmail.com>
2024-09-30 16:00:15 +0300

    hw-33 completed with lection

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 'use strict';
 
-const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+const IMG_FOLDER = './image/';
+const IMG_EXT = '.jpg';
 
-const random = Math.floor(Math.random() * images.length);
+// const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
 
-const randomImage = images[random];
+const getRanromNum = () => {
+  const random = Math.floor(Math.random() * 10);
+  return random;
+};
 
-console.log(randomImage);
+const generateImg = (imgName) => {
+  const newImage = document.createElement('img');
+  newImage.src = `${IMG_FOLDER}${imgName + IMG_EXT}.`;
+  return newImage;
+};
 
-const newImage = document.createElement('img');
-newImage.src = randomImage;
+const imgToAppemd = generateImg(getRanromNum());
+
+document.body.append(imgToAppemd);
