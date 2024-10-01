6abdb8120040771ec04629757b669b22adeaf0ef
Artem <artemshotskiy@gmail.com>
2024-10-01 13:23:28 +0300

    hw-34 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const p = document.querySelector('p');
+const ghost = document.querySelector('[data-p]');
+p.addEventListener('focus', (e) => {
+  ghost.style.display = 'block';
+});
+
+p.addEventListener('blur', (e) => {
+  ghost.style.display = 'none';
+});
