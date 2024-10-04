6cbfcfb79c5bd1f9dc3afe8648dbf85671d3633c
Artem <artemshotskiy@gmail.com>
2024-10-04 11:52:21 +0300

    hw-34 corected

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const input = document.querySelector('input');
-const ghost = document.querySelector('[data-p]');
-input.addEventListener('focus', (e) => {
-  ghost.style.display = 'block';
-});
+const createDiv = (text) => {
+  const div = document.createElement('div');
+  div.setAttribute('data-p', 'id');
+  div.innerHTML = text;
+  div.style.display = 'none';
+  document.body.appendChild(div);
+  return div;
+};
+const addEventListeners = (input, div) => {
+  input.addEventListener('focus', () => {
+    div.style.display = 'block';
+  });
+  input.addEventListener('blur', () => {
+    div.style.display = 'none';
+  });
+};
+const input1 = document.querySelector('#input-1');
+const unicDiv1 = createDiv('я тут. 1');
+addEventListeners(input1, unicDiv1);
 
-input.addEventListener('blur', (e) => {
-  ghost.style.display = 'none';
-});
+const input2 = document.querySelector('#input-2');
+const unicDiv2 = createDiv('я тут. 2');
+addEventListeners(input2, unicDiv2);
+
+const input3 = document.querySelector('#input-3');
+const unicDiv3 = createDiv('я тут. 3');
+addEventListeners(input3, unicDiv3);
