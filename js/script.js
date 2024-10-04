d6d4db39b98e8b9d7d80b61a7bd6766fee192d2d
Artem <artemshotskiy@gmail.com>
2024-10-04 12:40:19 +0300

    hw-32 corected

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
 'use strict';
 
-const tableBody = document.getElementById('table-body');
-let number = 1;
-for (let i = 0; i < 10; i++) {
-  const row = document.createElement('tr');
-  for (let j = 0; j < 10; j++) {
-    const value = document.createElement('td');
-    value.textContent = number;
-    row.append(value);
-    number++;
+const createTable = (rows = 10, columns = 10) => {
+  let number = 1;
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+  table.append(tbody);
+  for (let i = 1; i <= rows; i++) {
+    const row = document.createElement('tr');
+    for (let j = 1; j <= columns; j++) {
+      const column = document.createElement('td');
+      row.append(column);
+      column.textContent = number;
+      number++;
+    }
+    tbody.append(row);
   }
-  tableBody.append(row);
-}
-
-setTimeout(() => {
-  tableBody.style.backgroundColor = 'lightblue';
-}, 1000);
-setTimeout(() => {
-  tableBody.style.backgroundColor = 'pink';
-}, 2000);
-setTimeout(() => {
-  tableBody.style.backgroundColor = 'white';
-  tableBody.style.fontWeight = 'bold';
-}, 4000);
-
-const bodyEl = document.body;
-bodyEl.style.backgroundColor = 'purple';
+  return table;
+};
+console.log(createTable());
+document.body.appendChild(createTable());
