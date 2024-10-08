46dc23745bc4ef73c7b12efebbad98ffca915fcb
Artem <artemshotskiy@gmail.com>
2024-10-08 18:23:45 +0300

    hw-36 Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const generateList = (array) => {
+  const newList = (item) => {
+    const ul = document.createElement('ul');
+    item.forEach((item) => {
+      const li = document.createElement('li');
+      if (Array.isArray(item)) {
+        li.appendChild(newList(item));
+      } else {
+        li.textContent = item;
+      }
+      ul.appendChild(li);
+    });
+    return ul;
+  };
+  const result = newList(array);
+  document.body.appendChild(result);
+};
+
+generateList([1, 2, 3, [2, 3]]);
+
+const allElements = document.querySelectorAll('li, ul');
+console.log(allElements);
