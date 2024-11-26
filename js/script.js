e2317e5bfc98a4c359e4961c50f9df2097509c33
Artem <artemshotskiy@gmail.com>
2024-11-26 11:20:19 +0200

    hw-44 Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+class EmailService {
+    static instance = null;
+    #logs = [];
+
+    constructor() {
+        if (EmailService.instance) {
+            throw new Error('EmailService is Singleton.');
+        }
+        EmailService.instance = this;
+    }
+}
