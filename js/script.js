c1e50bcec732447b1a60e4878191494e2c46a169
Artem <artemshotskiy@gmail.com>
2024-11-29 15:44:42 +0200

    hw-45 Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,41 @@
 'use strict';
 
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+class TaskQueue {
+  constructor(delayTime) {
+    this.delayTime = delayTime;
+    this.tasks = [];
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+
+  run() {
+    let promise = Promise.resolve();
+    const results = [];
+    this.tasks.forEach((task) => {
+      promise = promise
+        .then(() => task())
+        .then((result) => {
+          console.log(result);
+          results.push(result);
+          return delay(this.delayTime);
+        });
+    });
+    return promise.then(() => 'Усі завдання завершено');
+  }
+}
+
+const queue = new TaskQueue(1000); // затримка між завданнями — 1 секунда
+
+queue.addTask(() => Promise.resolve('Task 1 виконано'));
+queue.addTask(() => Promise.resolve('Task 2 виконано'));
+queue.addTask(() => Promise.resolve('Task 3 виконано'));
+
+queue.run().then((message) => {
+  console.log(message); // Усі завдання завершено
+});
