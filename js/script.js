cf3496d4480af9bbeef8b4fb69876d0e8a549409
Artem <artemshotskiy@gmail.com>
2025-04-10 12:30:34 +0300

    hw-3 Done

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@ const numArray = [12, 2, 3, 5, 6, 7, 8, 9, 11];
 let sum = numArray.reduce((acc, num) => acc + num, 0);
 let averageSum = sum / numArray.length;
 
+
+
 alert('Cереднє значення ' + averageSum);
